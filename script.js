56e2ad3fb78b5e50cf8042a3ba1120f9510ca79b
Yogndr <106022206+Yogndr@users.noreply.github.com>
2024-12-28 09:16:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ let elem=document.getElementById("sampleTable");
 function insert_Row() {
 	let newrow=document.createElement("tr");
 	newrow.innerHTML="<td>New Cell1</td><td>New Cell2</td>"
-	elem.insertBefore(newrow,elem.childNodes[0]);
+	elem.insertBefore(newrow,elem.rows[0]);
   
 }
